29174f3dfcb7d395b5ad2ee44af4048df33c59d1
Kaden <kjrigsbee@gmail.com>
2023-12-05 14:45:50 -0700

    HAHA BYEBYE ROUTER

--- app/App.js
+++ app/App.js
@@ -1,9 +1,6 @@
 import { SnacksController } from './controllers/SnacksController.js';
-import { router } from './router-config.js';
 
 class App {
-
-  router = router
 
   SnacksController = new SnacksController
 }
